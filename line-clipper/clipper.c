0319514004c9ac80f16386c4b8e30aed220a91d0
Ivan K <ivan.kocienski@gmail.com>
2014-10-28 15:00:16 +0000

    line clipper

---
+++ line-clipper/clipper.c
@@ -0,0 +1,316 @@
+
+enum {
+  C_LEFT  = 8, // 1000
+  C_RIGHT = 4, // 0100
+  C_UP    = 2, // 0010
+  C_DOWN  = 1  // 0001
+};
+
+static int min_x;
+static int min_y;
+static int max_x;
+static int max_y;
+
+void setup_line_clipper( int minx, int miny, int maxx, int maxy ) {
+  min_x = minx;
+  min_y = miny;
+  max_x = maxx;
+  max_y = maxy;
+}
+
+#define dist( a, b, c, d ) ( (c-a * c-a) + (d-b * d-b) )
+
+int clip_line( int x1, int y1, int x2, int y2, int *ox1, int *oy1, int *ox2, int *oy2 ) {
+
+  char clip_a = 0;
+  char clip_b = 0;
+
+  if( x1 < min_x ) clip_a |= C_LEFT;
+  if( x2 < min_x ) clip_b |= C_LEFT;
+
+  if( y1 < min_y ) clip_a |= C_UP;
+  if( y2 < min_y ) clip_b |= C_UP;
+
+  if( x1 >= max_x ) clip_a |= C_RIGHT;
+  if( x2 >= max_x ) clip_b |= C_RIGHT;
+
+  if( y1 >= max_y ) clip_a |= C_DOWN;
+  if( y2 >= max_y ) clip_b |= C_DOWN;
+
+  if( (clip_a & clip_b) & C_LEFT  ) return 0;
+  if( (clip_a & clip_b) & C_DOWN  ) return 0;
+  if( (clip_a & clip_b) & C_UP    ) return 0;
+  if( (clip_a & clip_b) & C_RIGHT ) return 0;
+
+  float dx = (float)(x2 - x1);
+  float dy = (float)(y2 - y1);
+  float s1 = dy / dx; 
+  float s2 = dx / dy; 
+  int nx1, ny1;
+  int nx2, ny2;
+  int d1;
+  int d2;
+  
+  switch( clip_a ) {
+    case C_LEFT | C_UP: //okay
+      
+      //up
+      d1 = min_y - y1;
+      nx1 = x1 + s2 * d1;
+      ny1 = min_y;
+
+      //left
+      d2 = min_x - x1;
+      nx2 = min_x;
+      ny2 = y1 + s1 * d2;
+
+      if( dist(nx1, ny1, x2, y2) < dist(nx2, ny2, x2, y2) ) {
+        x1 = nx1;
+        y1 = ny1;
+      } else {
+        x1 = nx2;
+        y1 = ny2;
+      }
+      break; 
+
+    case C_LEFT: // okay
+      d1 = min_x - x1;
+      y1 += s1 * d1;
+      x1 = min_x;
+      break;
+
+    case C_LEFT | C_DOWN: // okay
+
+      // dpwn
+      d1 = max_y - y1;
+      nx1 = x1 + s2 * d1;
+      ny1 = max_y - 1;
+
+      // left
+      d2 = min_x - x1;
+      nx2 = min_x;
+      ny2 = y1 + s1 * d2;
+
+      if( dist(nx1, ny1, x2, y2) < dist(nx2, ny2, x2, y2) ) {
+        x1 = nx1;
+        y1 = ny1;
+      } else {
+        x1 = nx2;
+        y1 = ny2;
+      }
+
+      break;
+
+    case C_UP: // okay
+      d1 = min_y - y1;
+      x1 += s2 * d1;
+      y1 = min_y - 1; 
+      break;
+
+    case C_DOWN: // okay
+      d1 = max_y - y1;
+      x1 += s2 * d1;
+      y1 = max_y - 1;
+      break;
+
+    case C_RIGHT | C_UP:
+
+      // up
+      d1  = min_y - y1;
+      nx1 = x1 + s2 * d1;
+      ny1 = min_y; 
+
+      // right
+      d2  = x1 - max_x;
+      nx2 = max_x - 1;
+      ny2 = y1 - s1 * d2;
+
+      if( dist(nx1, ny1, x2, y2) > dist(nx2, ny2, x2, y2) ) {
+        x1 = nx1;
+        y1 = ny1;
+      } else {
+        x1 = nx2;
+        y1 = ny2;
+      }
+
+      break;
+
+    case C_RIGHT:
+      d1  = x1 - max_x;
+      y1 -= s1 * d1;
+      x1  = max_x - 1;
+      break;
+
+    case C_RIGHT | C_DOWN:
+
+      //right
+      d1  = x1 - max_x;
+      nx2 = max_x - 1;
+      ny2 = y1 - s1 * d1;
+
+      //down
+      d2 = max_y - y1;
+      nx1 = x1 + s2 * d2;
+      ny1 = max_y - 1;
+
+      if( dist(nx1, ny1, x2, y2) > dist(nx2, ny2, x2, y2) ) {
+        x1 = nx1;
+        y1 = ny1;
+      } else {
+        x1 = nx2;
+        y1 = ny2;
+      }
+      break;
+  }
+
+  // have we clipped ourselves to invisibiltiy?
+ 
+    if( clip_b & C_UP ) {
+      if( y1 < min_y ) return 0;
+    } else
+      if( clip_b & C_DOWN ) {
+	if( y1 >= max_y ) return 0;
+      }
+
+    if( clip_b & C_LEFT ) {
+      if( x1 < min_x ) return 0;
+    } else
+      if( clip_b & C_RIGHT ) {
+	if( x1 >= max_x ) return 0;
+      }
+    
+
+  switch( clip_b ) {
+    case C_LEFT | C_UP: //okay
+      
+      //up
+      d1 = min_y - y2;
+      nx1 = x2 + s2 * d1;
+      ny1 = min_y;
+
+      //left
+      d2 = min_x - x2;
+      nx2 = min_x;
+      ny2 = y2 + s1 * d2;
+
+      if( dist(nx1, ny1, x1, y1) < dist(nx2, ny2, x1, y1) ) {
+        x2 = nx1;
+        y2 = ny1;
+      } else {
+        x2 = nx2;
+        y2 = ny2;
+      }
+      break; 
+
+    case C_LEFT: // okay
+      d1 = min_x - x2;
+      y2 += s1 * d1;
+      x2 = min_x;
+      break;
+
+    case C_LEFT | C_DOWN: // okay
+
+      // dpwn
+      d1 = max_y - y2;
+      nx1 = x2 + s2 * d1;
+      ny1 = max_y - 1;
+
+      // left
+      d2 = min_x - x2;
+      nx2 = min_x;
+      ny2 = y2 + s1 * d2;
+
+      if( dist(nx1, ny1, x1, y1) < dist(nx2, ny2, x1, y1) ) {
+        x2 = nx1;
+        y2 = ny1;
+      } else {
+        x2 = nx2;
+        y2 = ny2;
+      }
+
+      break;
+
+    case C_UP: // okay
+      d1 = min_y - y2;
+      x2 += s2 * d1;
+      y2 = min_y - 1; 
+      break;
+
+    case C_DOWN: // okay
+      d1 = max_y - y2;
+      x2 += s2 * d1;
+      y2 = max_y - 1;
+      break;
+
+    case C_RIGHT | C_UP:
+
+      // up
+      d1  = min_y - y2;
+      nx1 = x2 + s2 * d1;
+      ny1 = min_y; 
+
+      // right
+      d2  = x2 - max_x;
+      nx2 = max_x - 1;
+      ny2 = y2 - s1 * d2;
+
+      if( dist(nx1, ny1, x1, y1) > dist(nx2, ny2, x1, y1) ) {
+        x2 = nx1;
+        y2 = ny1;
+      } else {
+        x2 = nx2;
+        y2 = ny2;
+      }
+
+      break;
+
+    case C_RIGHT:
+      d1  = x2 - max_x;
+      y2 -= s1 * d1;
+      x2  = max_x - 1;
+      break;
+
+    case C_RIGHT | C_DOWN:
+
+      //right
+      d1  = x2 - max_x;
+      nx2 = max_x - 1;
+      ny2 = y2 - s1 * d1;
+
+      //down
+      d2 = max_y - y2;
+      nx1 = x2 + s2 * d2;
+      ny1 = max_y - 1;
+
+      if( dist(nx1, ny1, x1, y1) > dist(nx2, ny2, x1, y1) ) {
+        x2 = nx1;
+        y2 = ny1;
+      } else {
+        x2 = nx2;
+        y2 = ny2;
+      }
+      break;
+
+  }
+
+    if( clip_a & C_UP ) {
+      if( y2 < min_y ) return 0;
+    } else
+      if( clip_a & C_DOWN ) {
+	if( y2 >= max_y ) return 0;
+      }
+
+    if( clip_a & C_LEFT ) {
+      if( x2 < min_x ) return 0;
+    } else
+      if( clip_a & C_RIGHT ) {
+	if( x2 >= max_x ) return 0;
+      }
+ 
+  *ox1 = x1;
+  *oy1 = y1;
+  *ox2 = x2;
+  *oy2 = y2;
+
+  return 1;
+}
---
+++ line-clipper/main.c
@@ -0,0 +1,130 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <time.h>
+
+#include "clipper.h"
+
+#define XRES 800
+#define YRES 600
+
+GLFWwindow* window;
+
+double line_x1 = XRES / 2;
+double line_y1 = YRES / 2; 
+double line_x2 = 0;
+double line_y2 = 0; 
+
+int clip_x1 = 200;
+int clip_y1 = 200;
+int clip_x2 = 600;
+int clip_y2 = 400;
+
+void do_stuff() {
+
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+
+  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+  glColor3f( 0.1, 0.1, 0.1 );
+  glBegin(GL_LINE_LOOP);
+  glVertex2f( clip_x1, clip_y1 );
+  glVertex2f( clip_x2, clip_y1 );
+  glVertex2f( clip_x2, clip_y2 );
+  glVertex2f( clip_x1, clip_y2 );
+  glEnd();
+
+
+  glColor3f( 0.5, 0, 0 );
+  glBegin(GL_LINES);
+  glVertex2f( line_x1, line_y1 );
+  glVertex2f( line_x2, line_y2 );
+  glEnd();
+
+  if( clip_line( line_x1, line_y1, line_x2, line_y2, &x1, &y1, &x2, &y2 ) ) {
+
+    glColor3f( 0, 1, 0 );
+    glBegin(GL_LINES);
+    glVertex2f( x1, y1 );
+    glVertex2f( x2, y2 );
+    glEnd();
+  }
+}
+
+void init_stuff() {
+
+  setup_line_clipper( clip_x1, clip_y1, clip_x2, clip_y2 );
+
+  glViewport(0, 0, XRES, YRES);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  glOrtho(0.0, (float)XRES, (float)YRES, 0.0, -1.0, 1.0);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+void cleanup() {
+  glfwTerminate();
+}
+
+int main( int argc, char** argv ) {
+
+  struct timespec ts;
+
+  ts.tv_sec  = 0;
+  ts.tv_nsec = 50 * 1000;
+
+  if(!glfwInit()) {
+    fprintf( stderr, "could not init glfw\n" );
+    return -1;
+  }
+
+  atexit( cleanup );
+
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  window = glfwCreateWindow( XRES, YRES, "line clipping", NULL, NULL );
+  if(!window) {
+    fprintf( stderr, "could not open window\n" );
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  init_stuff();
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window)) {
+
+    if( glfwGetMouseButton( window, GLFW_MOUSE_BUTTON_1 ) == GLFW_PRESS ) 
+      glfwGetCursorPos( window, &line_x1, &line_y1 );
+
+    if( glfwGetMouseButton( window, GLFW_MOUSE_BUTTON_2 ) == GLFW_PRESS ) 
+      glfwGetCursorPos( window, &line_x2, &line_y2 );
+    
+
+    do_stuff();
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+
+    nanosleep( &ts, NULL ); 
+  } 
+
+  init_stuff();
+
+  return 0;
+}
+
